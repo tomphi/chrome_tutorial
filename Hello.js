e3c0bda4aef1717bcb3d42249513317aa2abd09c
GildasTH <gildas.thiolon@gmail.com>
2016-06-19 15:30:55 +0000

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,7 @@
+//var inputString = prompt('Entrez un Chiffre :');
+var inputString = "639367";
+var inputInt=parseInt(inputString);
+var ChiffreString=["zero","un","deux","trois",'quatre',"cinq","six","sept","huit","neuf"];
+var PuissanceDix=["cent","mille","millions"];
+var nombreChiffre=inputString.length;
+alert(nombreChiffre);
