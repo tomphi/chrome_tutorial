1500559fd9a68f25e4152448a8458f865ea3536d
Thomas Philippe <Thomas Philippe>
2016-06-21 01:11:27 +0200

    Edit "popup.js"
    Now ask for a DateTime instead of a delay (much more convenient for the user)

--- script/popup.js
+++ script/popup.js
@@ -5,17 +5,37 @@ $(document).ready(function() {
 	
 	$('#BtnAlarmSetter').click(function() {
 		
-		// Alarm setting
-		var durationToSet = parseInt(prompt("Enter a duration (in minutes) after which an alarm will be set : "));
-	
+		// Alarm name
+		var alarmName = prompt("Nom de l'alarme : ");
+
+		// Date setting
+		var dateTimeToSet = prompt("Date et heure pour cette alarme (format : AAAA-MM-JJ-hh-mm) : ");
+		var arrayValues = dateTimeToSet.split('-');
+		var dateFormated = arrayValues[0] + "-" + arrayValues[1] + "-" + arrayValues[2] + "T" + arrayValues[3] + ":" + arrayValues[4] + ":00"; 
+		var durationToSet = Date.parse(dateFormated);
+
+		var dateActuelle = new Date(Date.now());
+
+		// Calcul du delai en minutes
+		var delay = (durationToSet - Date.now())/60000 + dateActuelle.getTimezoneOffset();
+
 		// EN MODE DEBUG
-		console.log(durationToSet);
+		console.log("date saisie : " + dateFormated);
+		console.log("date saisie en ms : " + durationToSet);
+		console.log("date actuelle en ms : " + Date.now());
+		console.log(alarmName + " sonnera dans " + delay + " minutes");
 
 		// Création de l'alarme
-		var myAlarm = chrome.alarms.create('Alarm_test', {delayInMinutes: durationToSet});
+		var myAlarm = chrome.alarms.create(alarmName, {delayInMinutes: delay});
 
-		// Fermeture de la fenêtre popup
-		window.close();
+		//On ajoute une notification de l'alarme dans le corps de la page popup
+		$('body').append('<p>Nouvelle alarme : ' + alarmName + ' Sonnera le ' + arrayValues[0] + "-" + arrayValues[1] + "-" + arrayValues[2] + ' &agrave; ' + arrayValues[3] + ":" + arrayValues[4] + '</p>');
+
+		// Fermeture de la fenêtre popup après un timer de 5s
+		window.setTimeout(function() {
+			window.close();
+		}, 5000);
+
 	});
 
 });
